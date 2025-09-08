2980584fc7e5101ba4938800a6392136578deb91
Ludovic <dormoy.dev@gmail.com>
2025-09-09 01:50:17 +0200

    Assign GMs, Officers, and Members in guild fixtures
    Update GuildMembershipFixtures to require at least 8 users, assigning 2 as GMs, 6 as Officers (3 per guild), and distributing remaining users as Members between two guilds. Improves fixture realism and role distribution for testing.

--- backend/src/DataFixtures/GuildMembershipFixtures.php
+++ backend/src/DataFixtures/GuildMembershipFixtures.php
@@ -18,26 +18,44 @@ class GuildMembershipFixtures extends Fixture implements DependentFixtureInterfa
 
     public function load(ObjectManager $manager): void
     {
-        $users = $this->userRepository->createQueryBuilder('u')
-            ->getQuery()
-            ->getResult();
+        $users = $this->userRepository->createQueryBuilder('u')->getQuery()->getResult();
 
-        if (count($users) < 2) {
-            throw new \RuntimeException('Il faut au moins 2 utilisateurs en base pour assigner 2 GM.');
+        if (count($users) < 8) {
+            throw new \RuntimeException('Need at least 8 users to assign 2 GMs and 6 Officers.');
         }
 
         shuffle($users);
-        $picked = array_slice($users, 0, 2);
 
         /** @var GameGuild $guild1 */
         $guild1 = $this->getReference('gm_guild_1', GameGuild::class);
-
         /** @var GameGuild $guild2 */
         $guild2 = $this->getReference('gm_guild_2', GameGuild::class);
 
-        foreach ([[$picked[0], $guild1], [$picked[1], $guild2]] as [$user, $guild]) {
-            $membership = new GuildMembership($user, $guild, GuildRole::GM);
-            $manager->persist($membership);
+        $gms = array_splice($users, 0, 2);
+        $manager->persist(new GuildMembership($gms[0], $guild1, GuildRole::GM));
+        $manager->persist(new GuildMembership($gms[1], $guild2, GuildRole::GM));
+
+        $officers = array_splice($users, 0, 6);
+        $officerChunks = array_chunk($officers, 3);
+        foreach ($officerChunks[0] as $user) {
+            $manager->persist(new GuildMembership($user, $guild1, GuildRole::OFFICER));
+        }
+        foreach ($officerChunks[1] as $user) {
+            $manager->persist(new GuildMembership($user, $guild2, GuildRole::OFFICER));
+        }
+
+        $members = $users;
+        if (!empty($members)) {
+            $memberChunks = array_chunk($members, (int) ceil(count($members) / 2));
+            $g1Members = $memberChunks[0] ?? [];
+            $g2Members = $memberChunks[1] ?? [];
+
+            foreach ($g1Members as $user) {
+                $manager->persist(new GuildMembership($user, $guild1, GuildRole::MEMBER));
+            }
+            foreach ($g2Members as $user) {
+                $manager->persist(new GuildMembership($user, $guild2, GuildRole::MEMBER));
+            }
         }
 
         $manager->flush();
